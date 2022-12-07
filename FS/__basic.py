0ba0d8cd41f67a3d8c69eb022ffdade5fde8ff81
Mike Tung <tongysmember@gmail.com>
2022-12-07 22:53:46 +0800

    [ADD] Transform_Function

--- FS/__basic.py
+++ FS/__basic.py
@@ -1,6 +1,6 @@
 import numpy as np
-from numpy.random import rand
-
+from numpy.random import rand 
+from FS.__tran_func import tran_func
 
 def init_position(lb, ub, N, dim):
     X = np.zeros([N, dim], dtype='float')
@@ -10,12 +10,12 @@ def init_position(lb, ub, N, dim):
     
     return X
 
-
-def binary_conversion(X, thres, N, dim):
+def binary_conversion(X, thres, N, dim, trans_func=tran_func.sl_trans):
     Xbin = np.zeros([N, dim], dtype='int')
     for i in range(N):
         for d in range(dim):
-            if X[i,d] > thres:
+            X_trans = trans_func(X[i,d])
+            if X_trans > thres:
                 Xbin[i,d] = 1
             else:
                 Xbin[i,d] = 0
@@ -28,5 +28,6 @@ def boundary(x, lb, ub):
         x = lb
     if x > ub:
         x = ub
-    
+
     return x
+
